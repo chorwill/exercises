0d154b175ad61f48b87d1e48b45e6b2988d3d43c
Chris Horwill <chris@stacktracehq.com>
2018-11-13 21:17:06 +1000

    correcting syntax

--- exercises.hs
+++ exercises.hs
@@ -29,3 +29,12 @@ changeMood _ = Blah
 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = 
+    if x > 0
+        then x
+        else -x
+
+f :: (a,b) -> (c,d) -> ((b,d), (a, c))
+f x y = ((snd x, snd y), (fst x, fst y))
