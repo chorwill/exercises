4aa0780cd2e8e81c3d12c9fcc04885c7bbbadb93
Chris Horwill <chris@stacktracehq.com>
2018-11-17 14:05:17 +1000

    Type Arguements

---
+++ chapter5/Exercises.hs
@@ -0,0 +1,11 @@
+module Exercises where
+
+-- 5.4 a) not b) length [a] -> Int c) concat [[a]] -> [a] d) head [a] -
+-- Type Arguements
+
+-- 1 a)
+f :: a -> a -> a -> a; f = undefined
+x :: Char; x = undefined
+
+-- 2 d)
+g :: a -> b -> c -> b; g = undefined
