5e04d1f5eaaaef7e9b8c9d2ab1f65c81252475a4
Chris Horwill <chris@stacktracehq.com>
2018-11-17 14:33:45 +1000

    before polymorphism

--- chapter5/Exercises.hs
+++ chapter5/Exercises.hs
@@ -9,3 +9,17 @@ x :: Char; x = undefined
 
 -- 2 d)
 g :: a -> b -> c -> b; g = undefined
+
+-- 3 d)
+h :: (Num a, Num b) => a -> b -> b; h = undefined
+
+-- 4 c)
+-- 5 a)
+jackal :: (Ord a, Eq b) => a -> b -> a; jackal = undefined 
+
+-- 6 e)
+-- 7 d)
+kessel :: (Ord a, Num b) => a -> b -> a; kessel = undefined
+
+-- 8 a)
+-- 9 c)
