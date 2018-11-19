7706bdbd58684da5b5d1ef9e545ebed2bd85967d
chorwill <chris@stacktracehq.com>
2018-11-20 08:43:41 +1000

    Determine the Type

---
+++ chapter5/chapter5Ex.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Chapter5Ex where
+
+-- 2. Num a => a
+
+-- x = 5
+-- y = x + 5
+-- w = y * 10
+
+-- 3. Num a => a -> a
+-- x = 5
+-- y = x + 5
+-- z y = y * 10
+
+-- 4. Fractional a => a
+-- x = 5
+-- y = x + 5
+-- f = 4 / y
+
+-- 5. [Char]
+x = "Julie"
+y = " <3 "
+z = "Haskell"
+f = x ++ y ++ z
+
+
--- chapter5/exercises.hs
+++ chapter5/exercises.hs
@@ -1,1 +1,25 @@
 module Exercises where
+
+-- 5.4 a) not b) length [a] -> Int c) concat [[a]] -> [a] d) head [a] -
+-- Type Arguements
+
+-- 1 a)
+f :: a -> a -> a -> a; f = undefined
+x :: Char; x = undefined
+
+-- 2 d)
+g :: a -> b -> c -> b; g = undefined
+
+-- 3 d)
+h :: (Num a, Num b) => a -> b -> b; h = undefined
+
+-- 4 c)
+-- 5 a)
+jackal :: (Ord a, Eq b) => a -> b -> a; jackal = undefined 
+
+-- 6 e)
+-- 7 d)
+kessel :: (Ord a, Num b) => a -> b -> a; kessel = undefined
+
+-- 8 a)
+-- 9 c)
