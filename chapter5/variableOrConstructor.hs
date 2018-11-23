93ead12618d1f3ffb6669588939927dc4cbb3d07
chorwill <chris@stacktracehq.com>
2018-11-23 15:50:52 +1000

    fixing?

--- chapter5/variableOrConstructor.hs
+++ chapter5/variableOrConstructor.hs
@@ -2,5 +2,30 @@
 
 module VariableOrConstructor where
 
+    functionH :: [a] -> a
+    functionH (x:_) = x
 
+    functionC :: Ord a => a -> a -> Bool
+    functionC x y =
+        if (x > y) then True else False
 
+    functionS :: (a, b) -> b
+    functionS (x, y) = y
+
+    myFunc :: (x -> y) -> (y -> z) -> c -> (a, x) -> (a, z)
+    myFunc xToY yToZ _ (a, x) = (a, (yToZ (xToY x)))
+
+    i :: a -> a
+    i x = x 
+
+    c :: a -> b -> a
+    c x y = x 
+
+    c'' :: b -> a -> b
+    c'' y x = y
+
+    c' :: a -> b -> b
+    c' x y = y
+
+    r :: [a] -> [a]
+    r listA =  listA
