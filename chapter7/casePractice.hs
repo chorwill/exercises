cb926bdbbc773d40b220b67c8792bfb1865ff1a5
chorwill <chris@stacktracehq.com>
2019-02-15 15:40:04 +1000

    higher order functions

--- chapter7/casePractice.hs
+++ chapter7/casePractice.hs
@@ -5,3 +5,14 @@ functionC x y =
         True -> x
         False -> y
     where greater = x > y
+
+ifEvenAdd2 n =
+    case n `rem` 2 of
+        0 -> n + 2
+        _ -> n
+
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        _ -> 0
---
+++ chapter7/higherOrder.hs
@@ -0,0 +1,3 @@
+module HigherOrder where
+
+    
