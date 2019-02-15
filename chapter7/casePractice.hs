94833cc5b183c4eb80c2c1cbe505301a75981dfd
chorwill <chris@stacktracehq.com>
2019-02-15 14:52:18 +1000

    don't remember when these are from

---
+++ chapter7/casePractice.hs
@@ -0,0 +1,7 @@
+module CasePractice where
+
+functionC x y =
+    case greater of
+        True -> x
+        False -> y
+    where greater = x > y
--- chapter7/varietyPack.hs
+++ chapter7/varietyPack.hs
@@ -4,8 +4,23 @@ module VarietyPack where
 -- b)  k2 :: ([Char], Num b => b) -> [Char]
 -- c) k1 and k3
 
-f :: (a, b, c)
--> (d, e, f)
--> ((a, d), (c, f))
+f :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
+f (a, _, c) (d, _, f) =  ((a, d) , (c, f))
 
-f (a, b, c) (d, e, f) =  ((a, d) , (c, f))
+funcZ x = 
+    case x + 1 == 1 of
+        True -> "AWESOME"
+        False -> "wut"
+
+pal xs =
+    case xs == reverse xs of
+        True -> "yes"
+        Flase -> "no"
+
+pal' xs =
+    case y of
+        True -> "yes"
+        False -> "no"
+    where y = xs == reverse xs
+
+    
